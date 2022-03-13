7bcf7add77d88bd62585f6e8bec2aadccd525f6d
MarineauAndreanne <83275409+MarineauAndreanne@users.noreply.github.com>
2022-03-13 10:15:24 -0400

    Integrated axios and dictionary API

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
+import "./Search.css";
 
 export default function Search() {
   const [word, setWord] = useState("");
 
+  function handleApiResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function searchWord(event) {
     event.preventDefault();
-    alert(word);
+    //documentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(apiUrl).then(handleApiResponse);
   }
 
   function handleWordChange(event) {
