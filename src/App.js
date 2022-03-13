3e11a5efee69e50f4e609b09048e4f363d42dcd9
MarineauAndreanne <83275409+MarineauAndreanne@users.noreply.github.com>
2022-03-13 09:34:20 -0400

    Imported logo in header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Hello from Andy</h1>
+        <img src={logo} className="Dictionary-logo" alt="Dictionary-logo" />
+
+        <h1>Dictionary App</h1>
       </header>
     </div>
   );
