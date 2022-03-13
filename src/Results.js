27d2482cf10056a50cbb8122a49986ef8f44afe1
MarineauAndreanne <83275409+MarineauAndreanne@users.noreply.github.com>
2022-03-13 13:11:56 -0400

    Installed font awesome

--- src/Results.js
+++ src/Results.js
@@ -6,7 +6,7 @@ export default function Results(props) {
     return (
       <div className="Resutls">
         <h2>{props.results.word}</h2>
-        <h3>{props.results.phonetic}</h3>
+        <h3>{props.results.phonetics[0].text}</h3>
         <a
           href={props.results.phonetics[0].audio}
           className="Audio-link"
